90b9af768592c34633ce515c01bfe4765af474fe
mpyltsin <mpyltsin@custis.ru>
2018-10-14 17:12:25 +0300

    end lesson 27

--- src/main/java/com/otus/jdbc/controller/WelcomeController.java
+++ src/main/java/com/otus/jdbc/controller/WelcomeController.java
@@ -1,6 +1,8 @@
 package com.otus.jdbc.controller;
 
+import io.micrometer.core.instrument.Counter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class WelcomeController {
 
+    private final Counter enterCounter;
+
+    @Autowired
+    public WelcomeController(Counter enterCounter) {
+        this.enterCounter = enterCounter;
+    }
+
     @RequestMapping("/")
     public String welcome() {
+        enterCounter.increment();
         return "index";
     }
 }
